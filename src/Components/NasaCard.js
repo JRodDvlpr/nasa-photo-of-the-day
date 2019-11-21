cc57448082ec6ecc7ea27a901c969583a3ae2a87
JRodDvlpr <Mialienproductions@gmail.com>
2019-11-21 17:09:19 -0500

    Added Fonts

--- src/Components/NasaCard.js
+++ src/Components/NasaCard.js
@@ -1,24 +1,20 @@
 // Nasa Card 
 import React from "react";
-import {
-  Card, CardImg, CardText, CardBody,
-  CardTitle, CardSubtitle
-} from 'reactstrap';
+
 
 
 // "HTML stlye" format that will append the data from the API
 const NasaCard = ({hdurl, title, text, date}) => {
   return (
-    
+  <div className="header">Nasa Photo Of The Day:
     <div className="space-Container">
-      <Card>
-        <CardImg src={hdurl} alt="spacePhoto"/>
-        <CardBody>
-          <CardTitle>{title}</CardTitle>
-            <CardSubtitle>{date}</CardSubtitle>
-          <CardText>{text}</CardText>
-        </CardBody>
-      </Card>
+      <img src={hdurl} alt="spacePhoto"/>
+        <div className="innertext">
+          <h2>{title}</h2>
+            <p>{date}</p>
+          <p>{text}</p>
+        </div>
+    </div>
     </div>
   );
 };
